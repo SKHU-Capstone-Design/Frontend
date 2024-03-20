b1c71f2057fcb2be88c2071ca6e871663e7f1b26
I, yoonah <qwer120542@gmail.com>
2024-03-20 12:26:48 +0900

    feat : make a roote page - close  #7

---
+++ src/Pages/Root.tsx
@@ -0,0 +1,22 @@
+import "../Styles/Root.less";
+import { Link } from 'react-router-dom';
+
+function Home() {
+
+        return (
+            <>
+                <div className="rootewrap">
+                    <div className="rootetxt">
+                        <p className="load">가장 넓은 길은</p>
+                        <p className="mind">언제나 내 마음속에</p>
+                        <p className="smalltxt">다이어리와 함께 걸어요</p>
+                        <div className="signup-link"> 
+                            <button><Link to="/Login">Let's go</Link></button> 
+                        </div>
+                    </div>
+                </div>    
+            </>
+        )
+    }
+  
+  export default Home;
--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Login from './Pages/Login.tsx';
-import Home from './Pages/Home.tsx';
+import Roote from './Pages/Roote.tsx';
 import Signup from './Pages/Signup.tsx';
 
 const root = ReactDOM.createRoot(document.getElementById('root')!);
@@ -10,7 +10,7 @@ root.render(
   <React.StrictMode>
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route path="/" element={<Roote />} />
         <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<Signup />} />
       </Routes>
