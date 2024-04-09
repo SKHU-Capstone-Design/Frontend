f6d74326aa7f44fd3cbe3d87c41c0eea04848c7d
I, yoonah <qwer120542@gmail.com>
2024-04-09 23:56:27 +0900

    Style: Redesign and Apply Styles to the Signup Page

--- src/Pages/Signup2.tsx
+++ src/Pages/Signup2.tsx
@@ -1,63 +1,75 @@
 import { useState } from "react";
-import { useNavigate } from "react-router-dom"; 
+import { useNavigate } from "react-router-dom";
+import "../Styles/Signup2.less";    
 
 function Signup2() {
     const [nickname, setNickname] = useState("");
     const [age, setAge] = useState("");
     const [gender, setGender] = useState("");
-    const [phoneNumber, setPhoneNumber] = useState("");
-
+    const [phonenum, setPhonenum] = useState("");
     const navigate = useNavigate(); 
+
+    const handleSubmit = (e: { preventDefault: () => void; }) => {
+        e.preventDefault();
+        navigate("/user/save3"); 
+    };
 
     const handlePrevious = () => {
         navigate("/user/save");
     };
 
-    const handleNext = () => {
-        navigate("/user/save3"); 
-    };
-
     return (
-        <div>
-            <p>회원가입</p>
-            <form>
-                <div>
-                    <input 
-                        type="text" 
-                        value={nickname} 
-                        onChange={(e) => setNickname(e.target.value)} 
-                        placeholder="닉네임을 입력하세요" 
-                    />
-                </div>
-                <div>
-                    <input 
-                        type="number" 
-                        value={age} 
-                        onChange={(e) => setAge(e.target.value)} 
-                        placeholder="나이를 입력하세요" 
-                    />
-                </div>
-                <div>
-                    <input 
-                        type="text" 
-                        value={gender} 
-                        onChange={(e) => setGender(e.target.value)} 
-                        placeholder="성별을 입력하세요" 
-                    />
-                </div>
-                <div>
-                    <input 
-                        type="tel" 
-                        value={phoneNumber} 
-                        onChange={(e) => setPhoneNumber(e.target.value)} 
-                        placeholder="휴대폰 번호를 입력하세요" 
-                    />
-                </div>
-                <div>
-                    <button type="button" onClick={handlePrevious}> 이전 </button> 
-                    <button type="button" onClick={handleNext}> 다음  </button> 
-                </div>
-            </form>
+        <div className="signupwrap0">    
+            <div className="signupwrap">
+                <p className="signuptext">회원가입</p>
+                <form onSubmit={handleSubmit} className="form-container">
+                    <div className="input-container">
+                        <input 
+                            type="text" 
+                            id="nickname"
+                            placeholder="닉네임"
+                            required
+                            value={nickname} 
+                            onChange={(e) => setNickname(e.target.value)} 
+                        />
+                    </div>
+                    <div className="input-container2">
+                        <input 
+                            type="number" 
+                            id="age"
+                            placeholder="나이"
+                            required
+                            value={age} 
+                            onChange={(e) => setAge(e.target.value)} 
+                        />
+                    </div>
+                    <div className="input-container2">
+                        <select
+                            id="gender"
+                            value={gender}
+                            onChange={(e) => setGender(e.target.value)} 
+                        >
+                            <option value="">성별을 선택해주세요</option>
+                            <option value="MALE">남성</option>
+                            <option value="FEMALE">여성</option>
+                        </select>
+                    </div>
+                    <div className="input-container2">
+                        <input 
+                            type="text" 
+                            id="phonenum"
+                            placeholder="휴대폰 번호"
+                            required
+                            value={phonenum} 
+                            onChange={(e) => setPhonenum(e.target.value)} 
+                        />
+                    </div>
+                    <div className="button-container2">
+                        <button type="button" onClick={handlePrevious} className="prebtn"> <p>&lt;</p> </button> 
+                        <button type="submit" className="nextbtn"> <p>&gt;</p>  </button> 
+                    </div>
+                </form>
+            </div>
         </div>
     );
 }
