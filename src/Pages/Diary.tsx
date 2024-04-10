07c90c2a92c91fdbd584818551ad0a11e85ceee5
I, yoonah <qwer120542@gmail.com>
2024-04-10 11:49:02 +0900

    feat : Design and implement the Avatar page.

---
+++ src/Pages/Diary.tsx
@@ -0,0 +1,14 @@
+import '../Styles/realhome.less';
+import Navbar from './Navbar';
+
+
+function Diary() {
+    return (
+        <div>
+            <Navbar />
+            <p>다이어리 페이지</p>
+        </div>
+    );
+}
+
+export default Diary;
