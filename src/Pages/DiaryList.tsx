feeaeac5751795a8619e025b9881295f6665b341
I, yoonah <qwer120542@gmail.com>
2024-04-28 17:23:33 +0900

    chore: edit diary annotation

--- src/Pages/DiaryList.tsx
+++ src/Pages/DiaryList.tsx
@@ -2,8 +2,8 @@ import '../Styles/DiaryList.less';
 import Navbar from './Navbar';
 import { Link, useLocation } from 'react-router-dom';
 
-function DiaryList() {
-  const location = useLocation(); // 컴포넌트 내부에서 useLocation 호출
+function DiaryList() { // 임시 데이터. 나중에는 사용자가 서버에 저장한 게시글이 이 리스트에 들어갈 것
+  const location = useLocation(); // 컴포넌트 내부에서 useLocation 호출. useLocation은 현재 URL을 반환하는 역할 수행
 
   const storedPosts = [
     { No: "1", title: "임시데이터" },
@@ -37,7 +37,7 @@ function DiaryTable({ storedPosts, location }: { storedPosts: { No: string; titl
         </tbody>
       </table>
       <div className='diary-write-btn'>
-        <Link to={`${location.pathname}/diary/save`}>
+        <Link to={`${location.pathname}/diary/save`}> {/*//useLocation으로 현재 URL을 받아온 뒤 파라미터 사용}*/}
           <button className="diary-write-button">글 작성</button>
         </Link> 
       </div>   
