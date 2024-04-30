6addcd0d1a9e56fe8895bd7f59b66dd4ed64a52f
I, yoonah <qwer120542@gmail.com>
2024-05-01 08:48:36 +0900

    feat: jwt login client processing

--- src/Pages/Login.tsx
+++ src/Pages/Login.tsx
@@ -1,29 +1,46 @@
 import { useState } from 'react';
-import { Link, useNavigate } from 'react-router-dom'; 
-import "../Styles/Login.less";
+import { useNavigate, Link } from 'react-router-dom'; 
+import axios from 'axios';
+import '../Styles/Login.less';
 
 function Login() {
-  const [userId, setUserId] = useState('');
+  const [email, setemail] = useState('');
   const [password, setPassword] = useState('');
   const navigate = useNavigate(); 
   const [loginActive, setLoginActive] = useState(false); 
+
   const handleInputChange = (event: { target: { name: any; value: any; }; }) => {
     const { name, value } = event.target;
-    if (name === 'username') {
-      setUserId(value);
+    if (name === 'email') {
+      setemail(value);
     } else if (name === 'password') {
       setPassword(value);
     }
     
-    if (userId !== '' && password !== '') {
+    if (email !== '' && password !== '') {
       setLoginActive(true);
     } else {
       setLoginActive(false);
     }
   }
 
-  const handleLogin = () => {
-    navigate('/user/home'); 
+  const handleLogin = async () => {
+    try {
+      const response = await axios.post('http://localhost:8080/login', {
+        email: email,
+        password: password
+      });
+
+      const accessToken = response.data.accessToken;
+
+      // 토큰을 로컬 스토리지에 저장
+      localStorage.setItem('accessToken', accessToken);
+
+      // 홈 페이지로 이동
+      navigate('/user/home'); 
+    } catch (error) {
+      console.error('로그인 실패:', error);
+    }
   }
 
   return (
@@ -32,18 +49,12 @@ function Login() {
       <div className="gray-box">
         <div className='idbox'>
           <label>
-            <input type="text" name="username" placeholder="아이디" value={userId} onChange={handleInputChange} />
+            <input type="text" name="email" placeholder="아이디" value={email} onChange={handleInputChange} />
           </label>
         </div>
         <div className='pwbox'>
           <label>
             <input type="password" name="password" placeholder="비밀번호" value={password} onChange={handleInputChange} />
-          </label>
-        </div>
-        <div className='loginsave'>
-          <label htmlFor="saveLogin">
-            <input type="radio" id="saveLogin" name="loginOption" />
-            간편로그인 정보 저장
           </label>
         </div>
         <button className={`loginbtn ${loginActive ? 'active' : ''}`} onClick={handleLogin}>로그인</button> 
