d52c0c0f56e7dba02c92fc90450d827d320ee897
I, yoonah <qwer120542@gmail.com>
2024-05-22 11:35:50 +0900

    feat: Signup page 1 validation function added

--- src/Pages/Signup.tsx
+++ src/Pages/Signup.tsx
@@ -2,7 +2,6 @@ import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import "../Styles/Signup.less"; 
 
-
 function Signup() {
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
@@ -12,6 +11,21 @@ function Signup() {
     const handleSubmit = (e: { preventDefault: () => void; }) => {
         e.preventDefault();
 
+        // 이메일 유효성 검사
+        const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/; 
+        const koreanCharRegex = /[ㄱ-ㅎ|ㅏ-ㅣ|가-힣]/; 
+        if (!emailRegex.test(email) || koreanCharRegex.test(email)) {
+            alert('올바르지 않은 형식입니다');
+            return;
+        }
+
+        // [비밀번호 유효성 검사]
+        // 길이에 따른 유효성 검사
+        if (password.length < 6) {
+            alert('올바르지 않은 형식입니다 (비밀번호는 6자 이상이어야 합니다)');
+            return;
+        }
+        // 비밀번호, 비밀번호 확인이 일치하는지 확인
         if (password !== confirmPassword) {
             alert("비밀번호가 일치하지 않습니다.");
             return;
@@ -22,6 +36,17 @@ function Signup() {
 
     const handlePrevious = () => {
         navigate("/user"); 
+    };
+
+
+    const handleEmailChange = (e: { target: { value: string; }; }) => {
+        const value = e.target.value;
+        const koreanCharRegex = /[ㄱ-ㅎ|ㅏ-ㅣ|가-힣]/; 
+        if (koreanCharRegex.test(value)) {
+            alert('올바르지 않은 형식입니다.');
+            return;
+        }
+        setEmail(value);
     };
 
     return (
@@ -36,7 +61,7 @@ function Signup() {
                             placeholder="이메일"
                             required
                             value={email} 
-                            onChange={(e) => setEmail(e.target.value)} 
+                            onChange={handleEmailChange} 
                         />
                     </div>
                     <div className="input-container">
