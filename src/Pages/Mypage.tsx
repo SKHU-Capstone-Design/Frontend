473122ddb6cfe4a0f7fe1912ed4f4a69c4aa68ed
I, yoonah <qwer120542@gmail.com>
2024-06-05 08:32:50 +0900

    fix : fix mypage user information screen output

--- src/Pages/Mypage.tsx
+++ src/Pages/Mypage.tsx
@@ -4,7 +4,7 @@ import '../Styles/realhome.less';
 import '../Styles/Avatar.less';
 import '../Styles/Mypage.less';
 import Navbar from './Navbar';
-import mypageImg from '../public/mypage.png'
+import mypageImg from '../public/mypage.png';
 
 function Mypage() {
     const [email, setEmail] = useState(""); 
@@ -21,12 +21,16 @@ function Mypage() {
                         Authorization: `Bearer ${localStorage.getItem('accessToken')}`
                     }
                 });
+                
                 const userInfo = response.data;
-                setEmail(userInfo.email);
-                setName(userInfo.name); 
-                setGender(userInfo.gender); 
-                setAge(userInfo.age); 
-                setPhone(userInfo.phone); 
+
+                /* 수정 전: input 요소의 값이 정의된 상태에서 정의되지 않은 상태로 변경되면서 경고가 발생하고, input요소에 사용자 정보가 출력되지 않음.
+                이를 방지하기 위해, undefined 또는 null 값을 빈 문자열로 대체. input 요소의 값이 항상 정의된 상태로 유지되도록 수정함. */
+                setEmail(userInfo.email || "");
+                setName(userInfo.name || ""); 
+                setGender(userInfo.gender || ""); 
+                setAge(userInfo.age || ""); 
+                setPhone(userInfo.phone || ""); 
             } catch (error) {
                 console.error('사용자 정보를 불러오는 중 에러 발생:', error);
             }
@@ -35,19 +39,18 @@ function Mypage() {
         fetchUserInfo();
     }, []);
 
-
     return (
         <div>
             <div className="realhomewrap">
                 <Navbar />
                 <div className='realhomewrap2'>
                     <div className='myavatar'>
-                    <img src={mypageImg} alt="avatar" />
+                        <img src={mypageImg} alt="avatar" />
                         <div className='inputss'>
                             <div className='inputs' id='inputs'>
                                 <label>이메일</label>
                                 <input
-                                    type="string"
+                                    type="text"
                                     id='useremail'
                                     value={email}
                                     readOnly
@@ -86,7 +89,7 @@ function Mypage() {
                                 <div className='inputs'>
                                     <label>번호</label>
                                     <input
-                                        type="number"
+                                        type="text"
                                         id='userPhone'
                                         value={phone}
                                         readOnly
