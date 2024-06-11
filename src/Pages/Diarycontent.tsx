d7ffb6a56873e79d374c86d994b70e36f58c5c72
I, yoonah <qwer120542@gmail.com>
2024-06-12 06:39:36 +0900

    style: My page information or membership withdrawal option design

--- src/Pages/Diarycontent.tsx
+++ src/Pages/Diarycontent.tsx
@@ -1,21 +1,72 @@
-import { useState, useRef } from 'react';
+import { useState, useRef, useEffect } from 'react';
+import { useParams } from 'react-router-dom';
+import axios from 'axios';
 import '../Styles/Diarycontent.less';
 import Navbar from './Navbar';
 import { BsPencil, BsTrash } from "react-icons/bs";
 
+interface DiaryParams {
+  [key: string]: string; 
+}
+
+interface DiaryData {
+  diaryId: string;
+  title: string;
+  body: string;
+}
+
 function Diarycontent() {
+  const { diaryId } = useParams<DiaryParams>(); 
   const [isEditMode, setIsEditMode] = useState(false);
   const titleRef = useRef<HTMLDivElement>(null);
   const contentRef = useRef<HTMLDivElement>(null);
-  const [title, setTitle] = useState('회대 최고의 수업 소프캡디');
-  const [content, setContent] = useState('나는 소프캡디 수업이 정말 좋다ㅎㅎ');
+  const [title, setTitle] = useState('');
+  const [content, setContent] = useState('');
 
-  const handleEditClick = () => {
-    setIsEditMode(!isEditMode);
-    if (isEditMode && titleRef.current && contentRef.current) {
-      setTitle(titleRef.current.innerText);
-      setContent(contentRef.current.innerText);
+  useEffect(() => {
+    // 서버로부터 다이어리 데이터를 불러옴
+    const fetchDiary = async () => {
+      try {
+        const response = await axios.get(`http://localhost:8080/diary/${diaryId}`, {
+          headers: {
+            Authorization: `Bearer ${localStorage.getItem('accessToken')}`
+          }
+        });
+        // 서버 응답 데이터를 상태에 설정
+        const diaryData: DiaryData = response.data;
+        setTitle(diaryData.title);
+        setContent(diaryData.body);
+      } catch (error) {
+        console.error('다이어리 데이터를 불러오는 데 실패했습니다.', error);
+      }
+    };
+
+    fetchDiary();
+  }, [diaryId]);
+
+  const handleEditClick = async () => {
+    if (isEditMode) {
+      if (titleRef.current && contentRef.current) {
+        const updatedTitle = titleRef.current.innerText;
+        const updatedContent = contentRef.current.innerText;
+        
+        try {
+          await axios.post(`http://localhost:8080/diary/save`, {
+            diaryId: diaryId,
+            title: updatedTitle,
+            body: updatedContent,
+            date: new Date().toISOString().split('T')[0] 
+          }, {
+            headers: {
+              Authorization: `Bearer ${localStorage.getItem('accessToken')}`
+            }
+          });
+        } catch (error) {
+          alert('다이어리 수정에 실패했습니다.');
+        }
+      }
     }
+    setIsEditMode(!isEditMode);
   };
 
   return (
@@ -25,14 +76,14 @@ function Diarycontent() {
         <div className='diarybackground'>
           <div className='dairycontent'>
             <div className='icons'>
-              <div className='icon' onClick={handleEditClick}> 
-                <BsPencil className='pencil'/> 
+              <div className='icon' onClick={handleEditClick}>
+                <BsPencil className='pencil' />
               </div>
               <div className='icon'>
                 <BsTrash className='trash' />
               </div>
             </div>
-            <div 
+            <div
               className='diary_title'
               contentEditable={isEditMode}
               ref={titleRef}
@@ -40,7 +91,7 @@ function Diarycontent() {
             >
               {title}
             </div>
-            <div 
+            <div
               className='diary_content'
               contentEditable={isEditMode}
               ref={contentRef}
